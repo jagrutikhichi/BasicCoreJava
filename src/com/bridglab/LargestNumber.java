57d652fec92647db2b8e53fe7d05957c33da552c
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-23 23:44:14 +0530

    Added: Largest Number Program

---
+++ src/com/bridglab/LargestNumber.java
@@ -0,0 +1,24 @@
+package com.bridglab;
+
+public class LargestNumber {
+	public static void main (String[] args)
+	{
+		int a = 4;
+		int b = 8;
+		int c = 7;
+	
+		if (a > b && a > c) 
+		{
+			System.out.println("a is greater");
+		}
+		else if (b > a && b > c ) 
+		{
+			System.out.println("b is greater");			
+		}
+		else 
+		{
+			System.out.println("c is greater");
+		}
+		
+	}
+}
